e2e4faab5190efabbb2495d77a1405902fd1c71b
Mallory S <110414321+mals1230@users.noreply.github.com>
2022-10-18 19:14:34 -0700

    Pushing to Heroku

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,6 +2,20 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+router.get('/', async (req, res) => {
+  try {
+    const newComment = await Comment.findAll({
+      ...req.body,
+      post_id: req.params.id,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newComment);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 router.post('/:id', withAuth, async (req, res) => {
   try {
     const newComment = await Comment.create({
